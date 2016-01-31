3861b956c5afd489d99a7520b2ed1f16bbdd603c
ciggbit <ciggbit@gmail.com>
2016-01-30 19:10:50 -0500

    added key input

--- Source/Main.hx
+++ Source/Main.hx
@@ -12,72 +12,71 @@ import openfl.ui.Keyboard;
 import ui.Console;
 
 class Main extends Sprite {
-	var console: Console;
-	var keys: Array<Bool>;
-	var fps: FPS;
-	var prevTime: Int;
+  var console: Console;
+  var fps: FPS;
+  var prevTime: Int;
 
-	public function new() {
-		super();
-		console = new Console();
-		console.update(0);
+  public function new() {
+    super();
+    console = new Console();
+    console.update(0);
 
-		#if js
-			haxe.Log.trace = function(v: Dynamic, ?i):Void {
-				var msg = if (i != null) i.fileName + ":" + i.lineNumber + ": " + v else v;
-				console.log(msg);
-				untyped __js__("console").log(msg);
-			}
-		#else
-			haxe.Log.trace = function(v: Dynamic, ?i):Void {
-				var msg = if (i != null) i.fileName + ":" + i.lineNumber + ": " + v else v;
-				console.log(msg);
-			}
-		#end
+    #if js
+      haxe.Log.trace = function(v: Dynamic, ?i):Void {
+        var msg = if (i != null) i.fileName + ":" + i.lineNumber + ": " + v else v;
+        console.log(msg);
+        untyped __js__("console").log(msg);
+      }
+    #else
+      haxe.Log.trace = function(v: Dynamic, ?i):Void {
+        var msg = if (i != null) i.fileName + ":" + i.lineNumber + ": " + v else v;
+        console.log(msg);
+      }
+    #end
 
-		for (i in 0...30) trace(i);
+    for (i in 0...30) trace(i);
 
-		init();
-	}
+    init();
+  }
 
-	public function init() {
-		keys = [];
-		prevTime = lime.system.System.getTimer();
+  public function init() {
+    Key_Input.Initialize();
+    prevTime = lime.system.System.getTimer();
 
-		var bitmap = new Bitmap(Assets.getBitmapData("assets/openfl.png"));
-		bitmap.x = (stage.stageWidth - bitmap.width) / 2;
-		bitmap.y = (stage.stageHeight - bitmap.height) / 2;
-		addChild(bitmap);
+    var bitmap = new Bitmap(Assets.getBitmapData("assets/openfl.png"));
+    bitmap.x = (stage.stageWidth - bitmap.width) / 2;
+    bitmap.y = (stage.stageHeight - bitmap.height) / 2;
+    addChild(bitmap);
 
-		fps = new FPS();
-		addChild(fps);
+    fps = new FPS();
+    addChild(fps);
 
-		addChild(console);
+    addChild(console);
 
-		stage.addEventListener(KeyboardEvent.KEY_DOWN, keyDown);
-		stage.addEventListener(KeyboardEvent.KEY_UP, keyUp);
-		stage.addEventListener(Event.ENTER_FRAME, update);
-	}
+    stage.addEventListener(KeyboardEvent.KEY_DOWN, Key_Down);
+    stage.addEventListener(KeyboardEvent.KEY_UP, Key_Up);
+    stage.addEventListener(Event.ENTER_FRAME, Update);
+  }
 
-	private function keyDown(event: KeyboardEvent):Void {
-		if (!keys[event.keyCode] && event.keyCode == Keyboard.BACKQUOTE) {
-			console.visible = !console.visible;
-		}
+  private function Key_Down(event: KeyboardEvent):Void {
+    keys[Key_Input.keys[event.keyCode]] = true;
 
-		keys[event.keyCode] = true;
-	}
+    if (!keys[event.keyCode] && event.keyCode == Keyboard.BACKQUOTE) {
+      console.visible = !console.visible;
+    }
+  }
 
-	private function keyUp(event: KeyboardEvent):Void {
-		keys[event.keyCode] = false;
-	}
+  private function Key_Up(event: KeyboardEvent):Void {
+    keys[event.keyCode] = false;
+  }
 
-	private function update(event: Event):Void {
-		var currentTime = lime.system.System.getTimer();
-		var delta = (currentTime - prevTime) / 1000.0;
-		prevTime = currentTime;
+  private function Update(event: Event):Void {
+    var currentTime = lime.system.System.getTimer();
+    var delta = (currentTime - prevTime) / 1000.0;
+    prevTime = currentTime;
 
-		var speed = 50;
+    var speed = 50;
 
-		console.update(delta);
-	}
+    console.update(delta);
+  }
 }
---
+++ Source/keyinput.hx
@@ -0,0 +1,8 @@
+package;
+
+class Key_Input {
+  public static keys : Vector<Bool>;
+  public static Initialize() {
+    keys = new Vector<Bool>(200);
+  }
+};

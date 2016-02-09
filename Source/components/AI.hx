2a07e6a91cc6a860bae7703807813d598c0451f4
hercludes <hercludes@gmail.com>
2016-02-09 17:08:41 -0500

    fixed style

--- Source/components/AI.hx
+++ Source/components/AI.hx
@@ -1,27 +1,27 @@
 package components;
 
 class AI {
-  private var _sight_radius: Int;
-  private var _sight_direction: Vector;
-  private var _last_known_enemy_position: Vector;
+  private var sight_radius: Int;
+  private var sight_direction: Vector;
+  private var last_known_enemy_position: Vector;
 
   public function new(_sight_radius:Int) {
-    Sightradius = _sight_radius;
-    sightDirection = {x: 0, y: 0};
+    sight_radius = _sight_radius;
+    sight_direction = {x: 0, y: 0};
   }
 
   public inline function set_last_known_enemy_position(x: Int, y: Int) {
-    if (_last_known_enemy_position == null )
-      _last_known_enemy_position = {x: x, y: y};
+    if (last_known_enemy_position == null )
+      last_known_enemy_position = {x: x, y: y};
   }
 
   public inline function clear_last_known_enemy_position(): Void {
-    _last_known_enemy_position = null;
+    last_known_enemy_position = null;
   }
 
 
   public inline function set_sight_direction(x: Int, y: Int) {
-    _sight_direction = {x: x, y: y};
-    Vector2.normalize(_sight_direction);
+    sight_direction = {x: x, y: y};
+    Vector2.normalize(sight_direction);
   }
 };

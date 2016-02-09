c90ec5ee6cf025f9f13d44ae66ee2e66a221457f
Aceeri <conmcclusk@gmail.com>
2016-02-09 14:36:06 -0800

    style fixes

--- Source/Interface.hx
+++ Source/Interface.hx
@@ -7,25 +7,25 @@ import openfl.Vector;
 import ui.UserInterface;
 
 class Interface {
-	private static var sprite: Sprite = new Sprite();
-	public static var list: Vector<UserInterface> = [];
+  private static var sprite: Sprite = new Sprite();
+  public static var list: Vector<UserInterface> = [];
 
-	public static function add_to(canvas: DisplayObjectContainer) {
-		canvas.addChild(sprite);
-	}
+  public static function add_to(canvas: DisplayObjectContainer) {
+    canvas.addChild(sprite);
+  }
 
-	public static function remove_from(canvas: DisplayObjectContainer) {
-		canvas.removeChild(sprite);
-	}
-	
-	public static function add_interface(user_interface: UserInterface) {
-		list.push(user_interface);
-		sprite.addChild(user_interface);
-	}
+  public static function remove_from(canvas: DisplayObjectContainer) {
+    canvas.removeChild(sprite);
+  }
+  
+  public static function add_interface(user_interface: UserInterface) {
+    list.push(user_interface);
+    sprite.addChild(user_interface);
+  }
 
-	public static function remove_interface(user_interface: UserInterface) {
-		list.slice(list.indexOf(user_interface));
-		sprite.removeChild(user_interface);
-	}
+  public static function remove_interface(user_interface: UserInterface) {
+    list.slice(list.indexOf(user_interface));
+    sprite.removeChild(user_interface);
+  }
 
 }
